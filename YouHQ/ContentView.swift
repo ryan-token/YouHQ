72a3f04398a5c2b26d28733f002162856981c90f
Ryan Token <ryantoken13@gmail.com>
2025-12-29 16:47:28 -0700

    Initial Commit

---
+++ YouHQ/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YouHQ
+//
+//  Created by Ryan Token on 12/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YouHQ/YouHQApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouHQApp.swift
+//  YouHQ
+//
+//  Created by Ryan Token on 12/29/25.
+//
+
+import SwiftUI
+
+@main
+struct YouHQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
