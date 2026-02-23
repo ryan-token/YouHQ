8973102122c883d01ee0baece0643b7980333a89
Ryan Token <ryan@ryantoken.com>
2026-02-23 11:08:28 -0700

    Make the label on LabeledField tappable to focus the content within

--- YouHQ/Shared Views/LabeledField.swift
+++ YouHQ/Shared Views/LabeledField.swift
@@ -10,13 +10,18 @@ import SwiftUI
 struct LabeledField<Content: View>: View {
 	let label: String
 	@ViewBuilder let content: () -> Content
+	@FocusState private var isFocused: Bool
 
 	var body: some View {
 		LabeledContent {
 			content()
+				.focused($isFocused)
 		} label: {
 			HQText(label)
 				.foregroundStyle(.secondary)
+				.onTapGesture {
+					isFocused = true
+				}
 		}
 	}
 }
